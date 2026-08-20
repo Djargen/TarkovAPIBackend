ca6909c142232b0797239903bf1b9d4ca04fa497
thijs-dery <thijs.dery@gmail.com>
2026-08-20 18:53:04 +0200

    Controller now uses DbContext to ensure there is always data (either local or tarkov.dev)

--- Tarkov.API/Controllers/TarkovController.cs
+++ Tarkov.API/Controllers/TarkovController.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Tarkov.API.Services;
+using Tarkov.Infrastructure.Data;
 
 namespace Tarkov.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class TarkovController(TarkovAPIService tarkovApiService) : ControllerBase
+    public class TarkovController(TarkovDbContext context) : ControllerBase
     {
         [HttpGet("items")]
         public async Task<IActionResult> GetItems()
         {
-            var items = await tarkovApiService.GetTarkovItemsAsync();
+            var items = await context.Items.ToListAsync();
 
             if (items == null)
             {
@@ -18,7 +20,7 @@ namespace Tarkov.API.Controllers
             }
 
             // Return all items as a collection
-            return Ok(items.Values);
+            return Ok(items);
         }
     }
 }
